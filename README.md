81f9c4922ee4a83da3f968e6f31d5ae56418ec4e
anson-bgl <anson.kan@bgl.hk>
2021-04-21 15:48:30 +0800

    converted script into ts and now the script accept command line arguments

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
-#amp-convertor
+# amp-convertor
 
-- `global-replace.js` is the convertor, it replaces normal tags into amp tags and clean up other invalid tags
+- `global-replace.js` is the convertor, it replaces normal tags into amp tags and clean up other invalid tags.
 - here has 4 `blog-{something}.html` which are simple some source copy of the webflow blog pages. Use global-replace.js to test against them.
-- since all JS is removed from the page, some `workarounds.css` and `workaround.html` are for the workaround
-- `common-head.html` includes amp-boilerplate and google font links
+- since all JS is removed from the page, some `workarounds.css` and `workaround.html` are for the workaround.
+- `common-head.html` includes amp-boilerplate and google font links.
 
 ```
-// to start local dev server
 cd amp-convertor
+
+npm i --g serve
+npm i
+npm run tsc
+
+// to start local dev server
 server
 
 // convert a page
-node global-replace.js
+node build/ts/amp-converter.js https://stg.cloudbet.com/en/blog <basic_auth_username> <basic_auth_password> ./build/amp/blog-home.amp.html
 ```
 
-To verify, go to, for example, http://localhost:5000/build/amp/blog-categories.amp#development=1. #development=1 is need to trigger the amp validation. Open browser developer console then you will see amp validation messages
+Inside of `amp-convertor.ts`, the `main` function return a amp html source as string by default. Also, accepting output file path as the 4th argument, so that you may verift the amp page locally.
+
+To verify, go to, for example, http://localhost:5000/build/amp/blog-categories.amp#development=1. #development=1 is need to trigger the amp validation. Open browser developer console then you will see amp validation messages.
--- amp-converter.ts
+++ amp-converter.ts
@@ -1,17 +1,19 @@
-const fs = require('fs')
-const { PurgeCSS } = require('purgecss')
-const axios = require('axios');
+import fs from 'fs';
+import { PurgeCSS } from 'purgecss';
+import axios from 'axios';
 
-function getNavInfo (html) {
+function getNavInfo(html: string): Array<Array<string>> {
   // since the workaround.html is for the nav overlay, and there might be change in nav links in different locale or translation, this part might helps
-  const navInfo = []
+  const navInfo: Array<Array<string>> = []
   const nav = html.match(/<nav\srole=.navigation.\sclass=.nav-menu-v1 w-nav-menu.>([\s\S]*?)<div\sclass=.nav-menu-small"><\/div><\/nav>/gm)
   if (nav && nav[0]) {
     const navLinks = nav[0].match(/<a\shref=(.+?)\sclass=.nav-link w-inline-block.><div(\sclass=.text-block-2.)*?>(.*?)<\/div><\/a>/g)
-    for (const link of navLinks) {
-      const match = link.match(/<a\shref=(.+?)\sclass=.nav-link w-inline-block.><div(\sclass=.text-block-2.)*?>(.*?)<\/div><\/a>/)
-      if (match) {
-        navInfo.push([match[1].replace(/['|"]/g, ''), match[3]])
+    if (navLinks) {
+      for (const link of navLinks) {
+        const match = link.match(/<a\shref=(.+?)\sclass=.nav-link w-inline-block.><div(\sclass=.text-block-2.)*?>(.*?)<\/div><\/a>/)
+        if (match) {
+          navInfo.push([match[1].replace(/['|"]/g, ''), match[3]])
+        }
       }
     }
   }
@@ -19,13 +21,13 @@ function getNavInfo (html) {
   return navInfo
 }
 
-async function getAmpCSS (html, additionalCSS) {
+async function getAmpCSS(html: string, additionalCSS: string): Promise<string> {
   const rawCSS = []
   const cssLinks = html.match(/<link\s*href="(.+)"\s*rel="stylesheet"\s*type="text\/css"\s*\/>/g);
   if (cssLinks) {
     for (let link of cssLinks) {
       const match = link.match(/<link\s*href="(.+)"\s*rel="stylesheet"\s*type="text\/css"\s*\/>/)
-      if (match[1]) {
+      if (match && match[1]) {
         const result = await axios.get(match[1])
         rawCSS.push(result.data)
       }
@@ -55,16 +57,35 @@ async function getAmpCSS (html, additionalCSS) {
     .replace(/!important/g, '')
 }
 
-async function main() {
-  const inputPath = './blog-post.html'
-  const outputPath = './build/amp/blog-post.amp.html'
-  const headPath = './common-head.html'
-  const workaroundHTMLPath = './workaround.html'
-  const workaroundCSSPath = './workaround.css'
-  const navLinkPath = './nav-link.html'
+async function main(url: string, username: string, password: string, outputPath: string): Promise<string> {
+  if (!url) {
+    throw new Error('URL is missing...')
+  }
+
+  // const inputPath = './blog-home.html'
+  // const outputPath = './build/amp/blog-home.amp.html'
+  const headPath: string = './common-head.html'
+  const workaroundHTMLPath: string = './workaround.html'
+  const workaroundCSSPath: string = './workaround.css'
+  const navLinkPath: string = './nav-link.html'
 
   // original HTML source
-  let html = fs.readFileSync(inputPath, { encoding: 'utf8', flag: 'r' });
+  // let html = fs.readFileSync(inputPath, { encoding: 'utf8', flag: 'r' });
+  let html: string = ''
+  try {
+    const pageResult = await axios.get(url, {
+      auth: {
+        username,
+        password
+      }
+    })
+    html = pageResult.data
+  } catch (error) {
+    console.log('--------------------')
+    console.log('Have troubles getting html source from the URL...')
+    console.error(error)
+    throw error
+  }
 
   const navInfo = getNavInfo(html)
 
@@ -72,9 +93,9 @@ async function main() {
   const ampHead = fs.readFileSync(headPath, { encoding: 'utf8', flag: 'r' });
 
   // since the original NAV button in mobile view is controlled by js, this is a css version of that
-  const workaroundCSS = fs.readFileSync(workaroundCSSPath, { encoding: 'utf8', flag: 'r' });
-  let workaroundHTML = fs.readFileSync(workaroundHTMLPath, { encoding: 'utf8', flag: 'r' });
-  let navLink = fs.readFileSync(navLinkPath, { encoding: 'utf8', flag: 'r' });
+  const workaroundCSS: string = fs.readFileSync(workaroundCSSPath, { encoding: 'utf8', flag: 'r' });
+  let workaroundHTML: string = fs.readFileSync(workaroundHTMLPath, { encoding: 'utf8', flag: 'r' });
+  let navLink: string = fs.readFileSync(navLinkPath, { encoding: 'utf8', flag: 'r' });
   if (navInfo) {
     const navLinksHTML = []
     for (const info of navInfo) {
@@ -85,10 +106,19 @@ async function main() {
   }
   html = html.replace(/(class=.button.subscribe-button.w-button.>[\s\S]*?<\/a><\/div>[\s\S]*?)<div.class="menu-button w-nav-button">[\s\S]*?<div class="menu-icon">[\s\S]*?<div class="menu-line-top"><\/div>[\s\S]*?<div class="menu-line-middle"><\/div>[\s\S]*?<div class="menu-line-bottom"><\/div>[\s\S]*?<\/div>[\s\S]*?<\/div>[\s\S]*?<\/div>/g, `$1${workaroundHTML}`)
 
-  const ampCSS = await getAmpCSS(html, workaroundCSS)
+  let ampCSS: string = ''
+  try {
+    ampCSS = await getAmpCSS(html, workaroundCSS)
+  } catch (error) {
+    console.log('--------------------')
+    console.log('Have troubles preparing purged css...')
+    console.log(error)
+    throw error;
+
+  }
 
   html = html.replace(/<html/g, '<html ⚡ ')
-  // html = html.replace(/<script[\s\S]*?<\/script>/gm, '')
+  html = html.replace(/<script[\s\S]*?<\/script>/gm, '')
   html = html.replace(/<link[\s\S]*?rel="stylesheet"\s*type="text\/css"\s*\/>/gm, '')
   html = html.replace(/<style[\s\S]*?<\/style>/gm, '')
   html = html.replace(/<form id="wf-form-Footer-Form"/gm, '<form id="wf-form-Footer-Form" action="/" target="_top" ')
@@ -100,7 +130,7 @@ async function main() {
   html = html.replace(/(social-icon.*?w-inline-block.><)img([\s\S]*?)\/>/g, '$1amp-img$2 height="18" width="18"></amp-img>')
   html = html.replace(/(class="button-icon.*?".*?)\/>/g, '$1 height="18" width="18"></amp-img>')
   html = html.replace(/(instagram-photo-link[\s\S]*?w-inline-block.><)img([\s\S]*?)\/>/g, '$1amp-img$2 width="50" height="50" heights="(min-width:500px) 100%, 100%"></amp-img>')
-  
+
   html = html.replace(/(class=.mini-icon-grey.*?)\/>/g, '$1 height="14" width="14"></amp-img>')
   html = html.replace(/(class=.nav-logo.*?)\/>/g, '$1 height="28"></amp-img>')
   html = html.replace(/(class=.footer-v1-logo.*?)\/>/g, '$1 height="43" width="213.02"></amp-img>')
@@ -108,20 +138,32 @@ async function main() {
   html = html.replace(/(class=.more-link-arrow-hover.*?)\/>/g, '$1 height="14" width="14"></amp-img>')
   html = html.replace(/(class=.more-link-arrow.*?)\/>/g, '$1 height="14" width="14"></amp-img>')
   html = html.replace(/(class=.category-arrow.*?)\/>/g, '$1 height="16" width="16"></amp-img>')
-  
+
   html = html.replace(/(x.svg.[\s\S]*?alt=.*?)\/>/g, '$1 height="12" width="12"></amp-img>')
   html = html.replace(/(class=.nav-arrow.*?)\/>/g, '$1 height="16" width="16"></amp-img>')
 
   html = html.replace(/<img/g, '<amp-img')
-  
+
   // have not found the regex bug yet, but something is causing the replace for .mini-icon-grey adding height and width into a link, so this will remove them
   html = html.replace(/(<link.rel=.prerender[\s\S]*?.)height[\s\S]*?width[\s\S]*?(>)/g, '$1$2')
 
   html = html.replace(/<\/head>/, `<style amp-custom>${ampCSS}</style>${ampHead}</head>`)
 
-  fs.writeFile(outputPath, html, { flag: 'w' }, function (error) {
-    if (error) console.error(error)
-  })
+  if (outputPath) {
+    try {
+      fs.writeFile(outputPath, html, { flag: 'w' }, function (error) {
+        if (error) console.error(error)
+      })
+    } catch (error) {
+      console.log('--------------------')
+      console.log('Have troubles writing output html into a file...')
+      console.error(error)
+    }
+  }
+  return html
 }
 
-main()
+// console.log(process.argv);
+// main('https://stg.cloudbet.com/en/blog', './build/amp/blog-home.amp.html')
+const args = process.argv.slice(2)
+main(args[0], args[1], args[2], args[3])
